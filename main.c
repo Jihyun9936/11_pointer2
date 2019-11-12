38f35fe511e6e49562348d1b638f76168f394615
Jihyun9936 <55350908+Jihyun9936@users.noreply.github.com>
2019-11-12 15:46:28 +0900

    Lab03

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//Lab01
+/*Lab01
 int main(int argc, char *argv[]) {
 	char *pc;
 	int *pi;
@@ -22,6 +22,22 @@ int main(int argc, char *argv[]) {
 	printf("after : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
 	return 0;
+}*/
+
+/*Lab03*/
+int main(){
+	char buffer[8];
+	double *pd;
+	int *pi;
+	
+	pd = (double *)buffer;
+	*pd = 3.14;
+	printf("%f\n", *(double *)buffer);
+	
+	pi = (int *)buffer;
+	*pi = 123;
+	*(pi+1) = 456;
+	printf("%d %d\n", *(int*)buffer, *((int*)buffer+1));
+	
+	return 0;
 }
-
-
